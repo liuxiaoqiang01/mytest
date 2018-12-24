f4c853a224de6cdf0576697aba672a256823094e
lxq <liuxiaoqiang@itcast.cn>
2018-12-24 22:44:57 +0800

    项目第一次提交

---
+++ demo-test/src/main/java/com/itcast/demo/Test.java
@@ -0,0 +1,15 @@
+package com.itcast.demo;
+
+/**
+ * @Package: com.itcast.demo
+ * @ClassName: CLASS_NAME
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @Author: LiuXiaoQiang
+ * @Date: Created in 2018/12/24 0024  时间: 22:31
+ * < >
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("==================");
+    }
+}
